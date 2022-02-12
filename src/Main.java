7e746f0deccb47ad426416d123fc234051bc0cfe
Matheus Guanaz <matheusguanaz@hotmail.com>
2022-02-12 13:45:23 -0300

    implementando método main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -9,18 +11,37 @@ public class Main {
         curso1.setTitulo("Curso Java");
         curso1.setDescricao("descrição curso java");
         curso1.setCargaHoraria(30);
-        System.out.println(curso1);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso Javascript");
         curso2.setDescricao("descrição curso javascript");
         curso2.setCargaHoraria(30);
-        System.out.println(curso2);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria de java");
         mentoria.setData(LocalDate.now());
-        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("bootcamp de java developer");
+        bootcamp.setDescricao("descrição bootcamp de java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMatheus = new Dev();
+        devMatheus.setNome("Matheus");
+        devMatheus.inscreverBootcamp(bootcamp);
+        devMatheus.progredir();
+        System.out.println("Conteúdos Inscritos\n" + devMatheus.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos\n" + devMatheus.getConteudosConcluidos());
+        System.out.println(devMatheus.calcularXP());
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos\n" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos\n" + devJoao.getConteudosConcluidos());
+
     }
 }
